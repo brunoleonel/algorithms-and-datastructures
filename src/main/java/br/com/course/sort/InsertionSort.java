75baaba500e52f25e04983cf3ec3da74404beada
Bruno Leonel <bruno_leonels@hotmail.com>
2022-06-07 23:10:52 -0300

    Selection and Insertion Sort

--- src/main/java/br/com/course/sort/InsertionSort.java
+++ src/main/java/br/com/course/sort/InsertionSort.java
@@ -1,15 +1,16 @@
 package br.com.course.sort;
 
-public class BubbleSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
-                }
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
             }
+            intArray[i] = newElement;
         }
 
         for (int i = 0; i < intArray.length - 1; i++) {
--- src/main/java/br/com/course/sort/SelectionSort.java
+++ src/main/java/br/com/course/sort/SelectionSort.java
@@ -1,15 +1,17 @@
 package br.com.course.sort;
 
-public class BubbleSort {
+public class SelectionSort {
     public static void main(String[] args) {
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
         for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
+            int largest = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
                 }
             }
+            swap(intArray, largest, lastUnsortedIndex);
         }
 
         for (int i = 0; i < intArray.length - 1; i++) {
