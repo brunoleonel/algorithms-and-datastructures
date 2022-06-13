d6b7c0c12bab46add8a62ea6dfe28c884a67d497
Bruno Leonel <bruno_leonels@hotmail.com>
2022-06-13 18:45:13 -0300

    QuickSort

---
+++ src/main/java/br/com/course/sort/QuickSort.java
@@ -0,0 +1,42 @@
+package br.com.course.sort;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        sort(intArray, 0, intArray.length);
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void sort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        sort(input, start, pivotIndex);
+        sort(input, pivotIndex + 1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            while (i < j && input[--j] >= pivot);
+            if (i < j) {
+                input[i] = input[j];
+            }
+            while (i < j && input[++i] <= pivot);
+            if (i < j) {
+                input[j] = input[i];
+            }
+        }
+        input[j] = pivot;
+        return j;
+    }
+}
